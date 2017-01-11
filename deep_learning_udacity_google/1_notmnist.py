b6b44466ab2661cc2bf50079d1aff6051d705008
jhui <jonathan@jonathanhui.com>
2017-01-10 22:04:46 -0800

    First commit

---
+++ deep_learning_udacity_google/1_notmnist.py
@@ -0,0 +1,155 @@
+# pip3 install matplotlib
+# pip3 install numpy
+# pip install --upgrade "ipython[all]"
+# pip3 install scipy
+# pip3 install sklearn
+# pip3 install Pillow
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import tarfile
+import pickle
+from IPython.display import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from urllib.request import urlretrieve
+
+# Config the matplotlib backend as plotting inline in IPython
+#%matplotlib inline
+
+tmp_dir = 'tmp'
+if not os.path.exists(tmp_dir):
+    os.makedirs(tmp_dir)
+os.chdir(tmp_dir)
+print("Change working directory to" , os.getcwd())
+
+url = 'http://commondatastorage.googleapis.com/books1000/'
+last_percent_reported = None
+
+
+def download_progress_hook(count, blockSize, totalSize):
+    """A hook to report the progress of a download. This is mostly intended for users with
+    slow internet connections. Reports every 5% change in download progress.
+    """
+    global last_percent_reported
+    percent = int(count * blockSize * 100 / totalSize)
+
+    if last_percent_reported != percent:
+        if percent % 5 == 0:
+            sys.stdout.write("%s%%" % percent)
+            sys.stdout.flush()
+        else:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+
+        last_percent_reported = percent
+
+
+def maybe_download(filename, expected_bytes, force=False):
+    """Download a file if not present, and make sure it's the right size."""
+    if force or not os.path.exists(filename):
+        print('\nAttempting to download:', filename)
+        filename, _ = urlretrieve(url + filename, filename, reporthook=download_progress_hook)
+        print('\nDownload Complete!')
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        raise Exception(
+            'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+
+test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
+train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+
+num_classes = 10
+np.random.seed(133)
+
+
+def maybe_extract(filename, force=False):
+    root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+    if os.path.isdir(root) and not force:
+        # You may override by setting force=True.
+        print('%s already present - Skipping extraction of %s.' % (root, filename))
+    else:
+        print('Extracting data for %s. This may take a while. Please wait.' % root)
+        tar = tarfile.open(filename)
+        sys.stdout.flush()
+        tar.extractall()
+        tar.close()
+    data_folders = [
+        os.path.join(root, d) for d in sorted(os.listdir(root))
+        if os.path.isdir(os.path.join(root, d))]
+    if len(data_folders) != num_classes:
+        raise Exception(
+            'Expected %d folders, one per class. Found %d instead.' % (
+                num_classes, len(data_folders)))
+    print(root, data_folders)
+    return data_folders
+
+
+test_folders = maybe_extract(test_filename)
+train_folders = maybe_extract(train_filename)
+
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0  # Number of levels per pixel.
+
+
+def load_letter(folder, min_num_images):
+    """Load the data for a single letter label."""
+    image_files = os.listdir(folder)
+    dataset = np.ndarray(shape=(len(image_files), image_size, image_size),
+                         dtype=np.float32)
+    print(folder)
+    num_images = 0
+    for image in image_files:
+        image_file = os.path.join(folder, image)
+        try:
+            image_data = (ndimage.imread(image_file).astype(float) -
+                          pixel_depth / 2) / pixel_depth
+            if image_data.shape != (image_size, image_size):
+                raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+            dataset[num_images, :, :] = image_data
+            num_images = num_images + 1
+        except IOError as e:
+            print('Could not read:', image_file, ':', e, '- it\'s ok, skipping.')
+
+    dataset = dataset[0:num_images, :, :]
+    if num_images < min_num_images:
+        raise Exception('Many fewer images than expected: %d < %d' %
+                        (num_images, min_num_images))
+
+    print('Full dataset tensor:', dataset.shape)
+    print('Mean:', np.mean(dataset))
+    print('Standard deviation:', np.std(dataset))
+    return dataset
+
+
+def maybe_pickle(data_folders, min_num_images_per_class, force=False):
+    dataset_names = []
+    for folder in data_folders:
+        set_filename = folder + '.pickle'
+        dataset_names.append(set_filename)
+        if os.path.exists(set_filename) and not force:
+            # You may override by setting force=True.
+            print('%s already present - Skipping pickling.' % set_filename)
+        else:
+            print('Pickling %s.' % set_filename)
+            dataset = load_letter(folder, min_num_images_per_class)
+            try:
+                with open(set_filename, 'wb') as f:
+                    pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+            except Exception as e:
+                print('Unable to save data to', set_filename, ':', e)
+
+    return dataset_names
+
+
+test_datasets = maybe_pickle(test_folders, 1800)
+train_datasets = maybe_pickle(train_folders, 45000)
+
+print('Testing datasets: ', test_datasets)
+print('Train datasets: ', train_datasets)
