ea3282f2b9c9c99935cd2e62a969223583a03dee
jhui <jonathan@jonathanhui.com>
2017-07-15 23:30:47 -0700

    cm

--- numpy_scipy_matplotlib/sample.py
+++ numpy_scipy_matplotlib/sample.py
@@ -1,17 +1,5 @@
 import numpy as np
 import matplotlib as plt
 
-size = 100
-sigma_x = 6.
-sigma_y = 2.
-
-x = np.linspace(-10, 10, size)
-y = np.linspace(-10, 10, size)
-
-x, y = np.meshgrid(x, y)
-z = (1/(2*np.pi*sigma_x*sigma_y) * np.exp(-(x**2/(2*sigma_x**2)
-     + y**2/(2*sigma_y**2))))
-
-plt.contourf(x, y, z, cmap='Blues')
-plt.colorbar()
-plt.show()
+a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [15, 16, 17, 18]])
+print(a[:,0:2])
